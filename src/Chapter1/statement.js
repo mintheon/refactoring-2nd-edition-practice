fa78c3fd7310e615245e329efbda60b6adf04128
mintheon <mintheon@mintheon.com>
2021-11-23 23:47:13 +0900

    [Chapter1] refactor: 중간 데이터 생성을 전담하는 로직 분리

--- src/Chapter1/statement.js
+++ src/Chapter1/statement.js
@@ -1,72 +1,76 @@
 function statement(invoice, plays) {
-  const statementData = {};
+  return renderPlainText(createStatementData(invoice, plays));
 
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+  function createStatementData(invoice, plays) {
+    const statementData = {};
 
-  return renderPlainText(statementData, plays);
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
 
-  function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance); //얕은 복사 수행
+    return statementData;
 
-    result.play = playFor(result); //중간 데이터에 연극 정보를 저장
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    function enrichPerformance(aPerformance) {
+      const result = Object.assign({}, aPerformance); //얕은 복사 수행
 
-    return result;
-  }
+      result.play = playFor(result); //중간 데이터에 연극 정보를 저장
+      result.amount = amountFor(result);
+      result.volumeCredits = volumeCreditsFor(result);
 
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      //비극
-      case 'tragedy':
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-
-      //희극
-      case 'comedy':
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
+      return result;
     }
 
-    return result;
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(aPerformance.audience - 30, 0);
-
-    if ('comedy' === aPerformance.play.type) {
-      volumeCredits += Math.floor(aPerformance.audience / 5);
+    function playFor(aPerformance) {
+      return plays[aPerformance.playID];
     }
 
-    return volumeCredits;
-  }
+    function amountFor(aPerformance) {
+      let result = 0;
 
-  function totalAmount(data) {
-    return data.performances.reduce((total, p) => total + p.amount, 0);
-  }
+      switch (aPerformance.play.type) {
+        //비극
+        case 'tragedy':
+          result = 40000;
+          if (aPerformance.audience > 30) {
+            result += 1000 * (aPerformance.audience - 30);
+          }
+          break;
 
-  function totalVolumeCredits(data) {
-    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+        //희극
+        case 'comedy':
+          result = 30000;
+          if (aPerformance.audience > 20) {
+            result += 10000 + 500 * (aPerformance.audience - 20);
+          }
+          result += 300 * aPerformance.audience;
+          break;
+
+        default:
+          throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
+      }
+
+      return result;
+    }
+
+    function volumeCreditsFor(aPerformance) {
+      let volumeCredits = 0;
+      volumeCredits += Math.max(aPerformance.audience - 30, 0);
+
+      if ('comedy' === aPerformance.play.type) {
+        volumeCredits += Math.floor(aPerformance.audience / 5);
+      }
+
+      return volumeCredits;
+    }
+
+    function totalAmount(data) {
+      return data.performances.reduce((total, p) => total + p.amount, 0);
+    }
+
+    function totalVolumeCredits(data) {
+      return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+    }
   }
 }
 
