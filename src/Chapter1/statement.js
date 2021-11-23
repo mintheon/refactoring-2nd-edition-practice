6da46129af224f4a8ecd6551004f810b9ef118c1
mintheon <mintheon@mintheon.com>
2021-11-23 22:32:50 +0900

    [Chapter1] refactor: volumeCredits로직을 totalVolumeCredits메서드로 분리 및 인라인 처리

--- src/Chapter1/statement.js
+++ src/Chapter1/statement.js
@@ -10,15 +10,19 @@ function statement(invoice, plays) {
     totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
-
   result += `총액: ${usd(totalAmount / 100)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
 
   return result;
+
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+
+    return volumeCredits;
+  }
 
   function volumeCreditsFor(aPerformance) {
     let volumeCredits = 0;
