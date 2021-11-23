8a7879a6053fb9d13075c4f1e928f9fac1b6b2a3
mintheon <mintheon@mintheon.com>
2021-11-23 22:15:58 +0900

    [Chapter1] refactor: volumeCredits 관련 로직을 volumeCreditsFor 메서드로 분리

--- src/Chapter1/statement.js
+++ src/Chapter1/statement.js
@@ -10,13 +10,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    //포인트 적립
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    //희극 관객 5명마다 추가 포인트 제공
-    if ('comedy' === playFor(perf).type) {
-      volumeCredits += Math.floor(perf.audience / 5);
-    }
-
+    volumeCredits += volumeCreditsFor(perf);
     //청구내역을 출력
     result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
@@ -28,6 +22,17 @@ function statement(invoice, plays) {
   result += `적립 포인트: ${volumeCredits}점\n`;
 
   return result;
+
+  function volumeCreditsFor(aPerformance) {
+    let volumeCredits = 0;
+    volumeCredits += Math.max(aPerformance.audience - 30, 0);
+
+    if ('comedy' === playFor(aPerformance).type) {
+      volumeCredits += Math.floor(aPerformance.audience / 5);
+    }
+
+    return volumeCredits;
+  }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
