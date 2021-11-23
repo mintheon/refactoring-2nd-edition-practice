e8c76681e7f132ed92a536b2d98c9501f7ba2329
mintheon <mintheon@mintheon.com>
2021-11-23 22:42:16 +0900

    [Chapter1] refactor: totalAmount 로직을 totalAmount 메서드로 분리

--- src/Chapter1/statement.js
+++ src/Chapter1/statement.js
@@ -1,5 +1,4 @@
 function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
@@ -7,13 +6,21 @@ function statement(invoice, plays) {
     result += `${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     }석)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${usd(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount() / 100)}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
 
   return result;
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+
+    return result;
+  }
 
   function totalVolumeCredits() {
     let volumeCredits = 0;
