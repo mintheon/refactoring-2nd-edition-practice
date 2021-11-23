e2a20ce21a8e677de9d7dd207021a6280c2af749
mintheon <mintheon@mintheon.com>
2021-11-24 00:44:37 +0900

    [Chapter1] refactor: htmlStatement 함수 추가 및 테스트 코드 작성

--- src/Chapter1/statement.js
+++ src/Chapter1/statement.js
@@ -1,7 +1,28 @@
 import createStatementData from './createStatementData';
 
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
 function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += '<table>\n';
+  result += '<tr><th>연극</th><th>좌석수</th><th>금액</th></tr>';
+
+  for (let perf of data.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+
+  result += '</table>\n';
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+
+  return result;
 }
 
 function renderPlainText(data, plays) {
@@ -16,14 +37,14 @@ function renderPlainText(data, plays) {
   result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
 
   return result;
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
-  }
 }
 
-module.exports = statement;
+function usd(aNumber) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
+}
+
+export { statement, htmlStatement };
--- test/Chapter1/Chapter1.test.js
+++ test/Chapter1/Chapter1.test.js
@@ -1,10 +1,10 @@
-const statement = require('../../src/Chapter1/statement');
+import { statement, htmlStatement } from '../../src/Chapter1/statement.js';
 
-const invoicesJson = require('./data/invoices.json');
-const playsJson = require('./data/plays.json');
+import invoicesJson from './data/invoices.json';
+import playsJson from './data/plays.json';
 
 describe('statement', () => {
-  test('다중 플레이, 단일 고객, 다중 좌석에 대한 설명을 일반 텍스트로 인쇄해야 한다.', () => {
+  test('다중 플레이, 단일 고객, 다중 좌석에 대한 설명을 일반 텍스트로 표시해야 한다.', () => {
     let expected =
       '청구 내역 (고객명: BigCo)\n' +
       'Hamlet: $650.00 (55석)\n' +
@@ -14,5 +14,18 @@ describe('statement', () => {
       '적립 포인트: 47점\n';
 
     expect(statement(invoicesJson[0], playsJson)).toEqual(expected);
+  });
+
+  test('html로 다중 연극, 단일 고객 및 다중 좌석에 대한 설명을 표시해야 한다.', () => {
+    let result = `<h1>청구 내역 (고객명: BigCo)</h1>\n`;
+    result += '<table>\n';
+    result += `<tr><th>연극</th><th>좌석수</th><th>금액</th></tr>  <tr><td>Hamlet</td><td>(55석)</td><td>$650.00</td></tr>\n`;
+    result += `  <tr><td>As You Like It</td><td>(35석)</td><td>$580.00</td></tr>\n`;
+    result += `  <tr><td>Othello</td><td>(40석)</td><td>$500.00</td></tr>\n`;
+    result += '</table>\n';
+    result += `<p>총액: <em>$1,730.00</em></p>\n`;
+    result += `<p>적립 포인트: <em>47</em>점</p>\n`;
+
+    expect(htmlStatement(invoicesJson[0], playsJson)).toEqual(result);
   });
 });
